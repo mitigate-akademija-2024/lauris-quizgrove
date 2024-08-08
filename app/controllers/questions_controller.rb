bac11751a4cfcbf58f60b89050795c150f709f9f
Lauris Rabenau <119971139+Mrpowersz@users.noreply.github.com>
2024-08-08 14:03:26 +0000

    Questions init

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,24 @@ class QuestionsController < ApplicationController
   def index
   end
 
-  def start
+  def create
+    @question = @quiz.question.new(question_attributes)
+    if @question.save
+      flash.notice = "Question was successfully created"
+      redirect_to quiz_url(@quiz)
   end
 
-  def test
+  def new
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.new
   end
+
+private
+
+def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
+
+def question_attributes
+  params.require(:question).permit(:question_text)  
 end

ac2c64b99cd60055f670d60e0c43a77c4dc3d408
Lauris Rabenau <119971139+Mrpowersz@users.noreply.github.com>
2024-08-09 11:05:31 +0000

    Added check boxes and updated answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,23 +7,30 @@ class QuestionsController < ApplicationController
   def create
     @question = @quiz.questions.new(question_params)
 
-    @question.answers.new answer_text: params[:question][:answer1]
-    @question.answers.new answer_text: params[:question][:answer2]
-    @question.answers.new answer_text: params[:question][:answer3]
-
-    if @question.save
-      flash.notice = "Question was successfully created."
-      redirect_to quiz_url(@quiz)
-    else
+    if params[:commit] == "add_answer"
+      @question.answers.new
       render :new, status: :unprocessable_entity
+    else
+
+      if @question.save
+        flash.notice = "Question was successfully created."
+        redirect_to quiz_url(@quiz)
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def new
     @question = @quiz.questions.new
     @question.answers.new
+  end
+
+  def add_answer
+    @question = @quiz.questions.new(question_params)
     @question.answers.new
-    @question.answers.new
+
+    render :new
   end
 
   private
@@ -33,6 +40,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:question_text)
+    params.require(:question).permit(:question_text, answers_attributes: [:id, :answer_text, :correct])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
-    validates :question_text, presence: true
-  
-    belongs_to :quiz
-    
-    has_many :answers, dependent: :destroy
-  end
+  validates :question_text, presence: true
+
+  belongs_to :quiz
+  has_many :answers
+  accepts_nested_attributes_for :answers
+end

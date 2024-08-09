176ae128e7cf987f49421114d9d4fd0370064efe
Lauris Rabenau <119971139+Mrpowersz@users.noreply.github.com>
2024-08-09 14:19:19 +0000

    Update init

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,16 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :answers, dependent: :destroy
   
-  accepts_nested_attributes_for :answers, allow_destroy: true
+  accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: proc { |attributes| attributes ['answer_text'].blank?}
+
+  validate :validate_answers
+
+  private
+
+  def validate_answers
+    errors.add(:answers, :too_few, message: "at least two answers needed") if answers.count < 2
+    errors.add(:answers, :too_few, message: "at least one correct answers needed") if answers.none? {|ans| ans.correct?}
+  end
+
+
 end
